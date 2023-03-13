9e49de5631581bce694c9fc30f3d5d48fbce54c4
Alexander A. Shvedov <60114847+a-shvedov@users.noreply.github.com>
2023-03-13 22:28:21 +0300

    Create

---
+++ res/nodejs/test-tcp-wrap-listen.1671151256.js
@@ -0,0 +1,2 @@
+require('assert')
+TCP=process.binding('tcp_wrap').TCP;process.binding('stream_wrap').r=TCP('')=function(){function t(){if(0){('')}}d=function(){if(0){((0));function e(){(0)}}}}('',function(){('')},function(){})
