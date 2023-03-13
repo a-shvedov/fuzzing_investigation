822e53d2087566c6cd78600fa378ce56ea820ded
Alexander A. Shvedov <60114847+a-shvedov@users.noreply.github.com>
2023-03-13 22:27:02 +0300

    Create

---
+++ res/nodejs/id_000001_sig_11_sync_node1970_src_003379.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+let port = 8080;
+let bytes = 10;
+
+''.at(bytes);
+
+const server = http.createServer(function() {
+  ({'':''});
+});
+
+server.listen(port, function() {
+  const agent = new http.Agent();
+
+  while (true) {
+    http.get({ agent }, function() {
+      ('', function() {
+        if (0) {
+          // do something
+        }
+      });
+    });
+
+    // set delay 100 milliseconds between iterations
+    setTimeout(function() {}, 1000);
+  }
+});
